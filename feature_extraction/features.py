6c0a7a6407adc0cfcc9df8b56f1dc12a403e2178
Christian Framing <cfshop@t-online.de>
2015-08-03 15:09:08 +0200

    Initial version of feature extraction

---
+++ feature_extraction/features.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding:utf-8
+import sys
+import os.path
+import pickle
+import overpass
+
+def get_osm_data(int_sit):
+    api = overpass.API()
+    search_str = '(way(%d);way(%d););(._;>>;);out;' % \
+                    (int_sit["entry_way"], int_sit["exit_way"])
+    result = api.Get(search_str)
+    return result["elements"]
+
+if __name__ == "__main__":
+    for fn in sys.argv[1:]:
+        fn = os.path.abspath(fn)
+        fp, fne = os.path.split(fn)
+        print 'Processing', fne
+        with open(fn, 'r') as f:
+            int_sit = pickle.load(f)
+        
