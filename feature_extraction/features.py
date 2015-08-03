cbb5f650c27c84202cd4237d656dbfcc3d918dc8
Christian Framing <cfshop@t-online.de>
2015-08-03 15:55:18 +0200

    Implemented a feature template

--- feature_extraction/features.py
+++ feature_extraction/features.py
@@ -5,12 +5,31 @@ import os.path
 import pickle
 import overpass
 
+# Features
+# - Straßenwinkel zueinander
+# - Geschwindigkeitsbegrenzung am Eingang ausgang
+# - Fahrstreifenwahl am Eingang/Ausgang
+#   + Angegeben durch Abstand der Fahrstreifenmitte von Mittellinie der Straße in Openstreetmap
+# - Ist Eingang oder Ausgang Einbahnstraße?
+features = {
+    "street_angle": None,
+    "maxspeed_entry": None,
+    "maxspeed_exit": None,
+    "lane_distance_entry": None,
+    "lane_distance_exit": None,
+    "oneway_entry": None,
+    "oneway_exit": None
+}
+
 def get_osm_data(int_sit):
     api = overpass.API()
     search_str = '(way(%d);way(%d););(._;>>;);out;' % \
                     (int_sit["entry_way"], int_sit["exit_way"])
     result = api.Get(search_str)
     return result["elements"]
+
+def get_street_angle():
+    pass
 
 if __name__ == "__main__":
     for fn in sys.argv[1:]:
@@ -19,4 +38,6 @@ if __name__ == "__main__":
         print 'Processing', fne
         with open(fn, 'r') as f:
             int_sit = pickle.load(f)
+        osm = get_osm_data(int_sit)
+        
         
