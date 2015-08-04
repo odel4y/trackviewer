fed7ddebfb72ded1219d4dc44072b0433d6e6049
Christian Framing <cfshop@t-online.de>
2015-08-04 10:20:35 +0200

    Changed direction of ways facing the intersection

--- feature_extraction/features.py
+++ feature_extraction/features.py
@@ -60,25 +60,26 @@ def get_line_string_from_node_ids(osm, nodes):
     return shapely.LineString(coords)
     
 def get_way_line_strings(int_sit, osm):
-    """Return LineStrings for the actual entry and exit way separated by the intersection node both facing away from the intersection"""
+    """Return LineStrings for the actual entry and exit way separated by the intersection node.
+    The entry way faces towards the intersection and the exit way faces away from the intersection."""
     entry_way = get_element_by_id(osm, int_sit["entry_way"])
     entry_way_node_ids = entry_way["nodes"]
     if int_sit["entry_way"] == int_sit["exit_way"]: # One way for entry and exit -> split at intersection node
         cut_index = entry_way_node_ids.index(int_sit["intersection_node"])
         if entry_way_node_ids.index(int_sit["entry_way"]) < cut_index:
-            entry_way_line_string = get_line_string_from_node_ids(osm, reversed(entry_way_node_ids[:cut_index+1]))
+            entry_way_line_string = get_line_string_from_node_ids(osm, entry_way_node_ids[:cut_index+1])
             exit_way_line_string = get_line_string_from_node_ids(osm, entry_way_node_ids[cut_index:])
         else:
-            entry_way_line_string = get_line_string_from_node_ids(osm, entry_way_node_ids[cut_index:])
+            entry_way_line_string = get_line_string_from_node_ids(osm, reversed(entry_way_node_ids[cut_index:]))
             exit_way_line_string = get_line_string_from_node_ids(osm, reversed(entry_way_node_ids[:cut_index+1]))
     else:
         exit_way = get_element_by_id(osm, int_sit["exit_way"])
         entry_way_node_ids = entry_way["nodes"]
         exit_way_node_ids = exit_way["nodes"]
         if int_sit["intersection_node"] == entry_way_node_ids[0]:
-            entry_way_line_string = get_line_string_from_node_ids(osm, entry_way_node_ids)
-        else:
             entry_way_line_string = get_line_string_from_node_ids(osm, reversed(entry_way_node_ids))
+        else:
+            entry_way_line_string = get_line_string_from_node_ids(osm, entry_way_node_ids)
         if int_sit["intersection_node"] == exit_way_node_ids[0]:
             exit_way_line_string = get_line_string_from_node_ids(osm, exit_way_node_ids)
         else:
