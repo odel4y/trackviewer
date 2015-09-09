6e6de7ed79e00671250bfbda48b21d758b5d2ba5
Christian Framing <cfshop@t-online.de>
2015-09-09 19:01:19 +0200

    Testing features with cross validations sets

--- automatic_test.py
+++ automatic_test.py
@@ -45,6 +45,25 @@ def get_partitioned_samples(samples, train_ratio):
     test_samples = [samples[i] for i in test_indices]
     return train_samples, test_samples
 
+def get_cross_validation_samples(samples, train_ratio, number):
+    """Get several non-overlapping randomized train and test sets for cross validation"""
+    sample_count = len(samples)
+    print "Total number of samples:", sample_count
+    train_sample_count = int(round(sample_count * train_ratio))
+    indices = range(sample_count)
+    random.shuffle(indices)
+    test_sample_count = sample_count - train_sample_count
+    train_samples = []
+    test_samples = []
+    for i in range(number):
+        i1 = int((sample_count - test_sample_count)/number)*i
+        i2 = i1 + test_sample_count
+        test_indices = indices[i1:i2]
+        train_indices = indices[:i1] + indices[i2:]
+        test_samples.append([samples[i] for i in test_indices])
+        train_samples.append([samples[i] for i in train_indices])
+    return train_samples, test_samples
+
 def train(algorithms, train_samples):
     for algo in algorithms:
         algo.train(train_samples)
@@ -92,42 +111,52 @@ def test_plot(algorithms, test_samples):
         plot_intersection(s['geometry']['entry_line'], s['geometry']['exit_line'],\
                         s['geometry']['curve_secant'], s['geometry']['track_line'], predicted_lines)
 
-def test_feature_permutations(algo_class, train_samples, test_samples, features, min_num_features=4):
+def test_feature_permutations(algo_class, train_sample_sets, test_sample_sets, features, min_num_features=4, cross_validation=False):
     # Build feature sets
     feature_sets = []
     results = []
     rating_arg = 'average_mse'
+    feature_quality_dicts = []
+    if not cross_validation:
+        train_sample_sets = [train_sample_sets]
+        test_sample_sets = [test_sample_sets]
     for num_features in range(min_num_features, len(features)):
         it = itertools.combinations(features, num_features)
         it_list = list(it)
         feature_sets.extend(it_list)
-    print "Testing %d different feature sets" % len(feature_sets)
-    for i, f_set in enumerate(feature_sets):
-        algo = algo_class(f_set)
-        train([algo], train_samples)
-        result = test([algo], test_samples, console=False)
-        print "Algorithm %d/%d has score: %.2f" % (i+1, len(feature_sets), result[algo][rating_arg])
-        results.extend(result.items())
-    # Find Algorithms with best performance
-    results = sorted(results, key=lambda r: r[1][rating_arg])
-    # Calculate feature importance
-    feature_occurences = {}
-    for res in results:
-        features = res[0].features
-        for f in features:
-            if f in feature_occurences:
-                feature_occurences[f].append(res[1][rating_arg])
-            else:
-                feature_occurences[f] = [res[1][rating_arg]]
-    feature_importance = {}
-    for k,v in feature_occurences.iteritems():
-        feature_importance[k] = sum(v)/len(v)
-    sorted_feature_importance = sorted(feature_importance.items(), key=lambda it: it[1])
-    print "====== RESULTS ======"
-    for i in range(10):
-        print "====== #%d Algorithm: %.2f ======" % (i+1, results[i][1][rating_arg])
-        print '- ' + '\n- '.join(results[i][0].features)
-    print "====== FEATURE IMPORTANCE ======"
+    for train_samples, test_samples in zip(train_sample_sets, test_sample_sets):
+        print "Testing %d different feature sets" % len(feature_sets)
+        for i, f_set in enumerate(feature_sets):
+            algo = algo_class(f_set)
+            train([algo], train_samples)
+            result = test([algo], test_samples, console=False)
+            print "Algorithm %d/%d has quality: %.2f" % (i+1, len(feature_sets), result[algo][rating_arg])
+            results.extend(result.items())
+        # # Find Algorithms with best performance
+        # results = sorted(results, key=lambda r: r[1][rating_arg])
+        # Calculate feature importance
+        feature_occurences = {}
+        for res in results:
+            features = res[0].features
+            for f in features:
+                if f in feature_occurences:
+                    feature_occurences[f].append(res[1][rating_arg])
+                else:
+                    feature_occurences[f] = [res[1][rating_arg]]
+        feature_quality = {}
+        for k,v in feature_occurences.iteritems():
+            feature_quality[k] = sum(v)/len(v)
+        feature_quality_dicts.append(feature_quality)
+    total_feature_quality = {}
+    for f in feature_quality_dicts[0].keys():
+        f_scores = [res[f] for res in feature_quality_dicts]
+        total_feature_quality[f] = sum(f_scores)/len(f_scores)
+    sorted_feature_importance = sorted(total_feature_quality.items(), key=lambda it: it[1])
+    # print "====== RESULTS ======"
+    # for i in range(10):
+    #     print "====== #%d Algorithm: %.2f ======" % (i+1, results[i][1][rating_arg])
+    #     print '- ' + '\n- '.join(results[i][0].features)
+    print "====== FEATURE QUALITY ======"
     for i, (f, score) in enumerate(sorted_feature_importance):
         print "#%d %s: %.2f" % (i+1, f, score)
 
