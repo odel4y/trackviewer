fbb2549d49c11556c6173e137638a01a59e9d1bf
Christian Framing <cfshop@t-online.de>
2015-09-09 17:09:36 +0200

    Automated feature quality testing

--- automatic_test.py
+++ automatic_test.py
@@ -9,6 +9,7 @@ from sklearn.metrics import mean_squared_error
 import sklearn.preprocessing
 import random
 import pickle
+import itertools
 
 class PredictionAlgorithm(object):
     __metaclass__ = ABCMeta
@@ -48,7 +49,7 @@ def train(algorithms, train_samples):
     for algo in algorithms:
         algo.train(train_samples)
 
-def test(algorithms, test_samples):
+def test(algorithms, test_samples, console=True):
     results = {}
     for algo in algorithms:
         cumulated_mse = 0.
@@ -69,11 +70,12 @@ def test(algorithms, test_samples):
                 max_mse = max(max_mse, mse)
             else:
                 max_mse = mse
-        print 'Test with algorithm:', algo.get_name()
-        print 'Cumulated MSE:', cumulated_mse
-        print 'Average MSE:', average_mse
-        print 'Minimum MSE:', min_mse
-        print 'Maximum MSE:', max_mse
+        if console:
+            print 'Test with algorithm:', algo.get_name()
+            print 'Cumulated MSE:', cumulated_mse
+            print 'Average MSE:', average_mse
+            print 'Minimum MSE:', min_mse
+            print 'Maximum MSE:', max_mse
         results[algo] = {   'cumulated_mse': cumulated_mse,
                             'average_mse': average_mse,
                             'min_mse': min_mse,
@@ -89,6 +91,45 @@ def test_plot(algorithms, test_samples):
                         s['X'][_feature_types.index('intersection_angle')]))
         plot_intersection(s['geometry']['entry_line'], s['geometry']['exit_line'],\
                         s['geometry']['curve_secant'], s['geometry']['track_line'], predicted_lines)
+
+def test_feature_permutations(algo_class, train_samples, test_samples, features, min_num_features=4):
+    # Build feature sets
+    feature_sets = []
+    results = []
+    rating_arg = 'average_mse'
+    for num_features in range(min_num_features, len(features)):
+        it = itertools.combinations(features, num_features)
+        it_list = list(it)
+        feature_sets.extend(it_list)
+    print "Testing %d different feature sets" % len(feature_sets)
+    for i, f_set in enumerate(feature_sets):
+        algo = algo_class(f_set)
+        train([algo], train_samples)
+        result = test([algo], test_samples, console=False)
+        print "Algorithm %d/%d has score: %.2f" % (i+1, len(feature_sets), result[algo][rating_arg])
+        results.extend(result.items())
+    # Find Algorithms with best performance
+    results = sorted(results, key=lambda r: r[1][rating_arg])
+    # Calculate feature importance
+    feature_occurences = {}
+    for res in results:
+        features = res[0].features
+        for f in features:
+            if f in feature_occurences:
+                feature_occurences[f].append(res[1][rating_arg])
+            else:
+                feature_occurences[f] = [res[1][rating_arg]]
+    feature_importance = {}
+    for k,v in feature_occurences.iteritems():
+        feature_importance[k] = sum(v)/len(v)
+    sorted_feature_importance = sorted(feature_importance.items(), key=lambda it: it[1])
+    print "====== RESULTS ======"
+    for i in range(10):
+        print "====== #%d Algorithm: %.2f ======" % (i+1, results[i][1][rating_arg])
+        print '- ' + '\n- '.join(results[i][0].features)
+    print "====== FEATURE IMPORTANCE ======"
+    for i, (f, score) in enumerate(sorted_feature_importance):
+        print "#%d %s: %.2f" % (i+1, f, score)
 
 def load_samples(fn):
     with open(fn, 'r') as f:
