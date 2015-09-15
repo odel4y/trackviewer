185c9930194bf3134ab4a0c4b69d225d28489a32
Christian Framing <cfshop@t-online.de>
2015-09-15 15:16:01 +0200

    Added feature tests in subdirectory

---
+++ tests/test2.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#coding:utf-8
+# Simple test with RandomForestRegressor and RandomForestClassifier
+import automatic_test
+import regressors
+import reference_implementations
+
+feature_list = [
+    "intersection_angle",                       # Angle between entry and exit way
+    "maxspeed_entry",                           # Allowed maximum speed on entry way
+    "maxspeed_exit",                            # Allowed maximum speed on exit way
+    # "lane_distance_entry_exact",                # Distance of track line to curve secant center point at 0 degree angle
+    # "lane_distance_exit_exact",                 # Distance of track line to curve secant center point at 180 degree angle
+    "lane_distance_entry_lane_center",          # Distance of lane center line to curve secant ceter point at 0 degree angle
+    "lane_distance_exit_lane_center",           # Distance of lane center line to curve secant ceter point at 180 degree angle
+    "oneway_entry",                             # Is entry way a oneway street?
+    "oneway_exit",                              # Is exit way a oneway street?
+    "vehicle_speed_entry",                      # Measured vehicle speed on entry way at INT_DIST
+    "vehicle_speed_exit"                        # Measured vehicle speed on exit way at INT_DIST
+]
+rf_algo = regressors.RandomForestAlgorithm(feature_list)
+rfc_algo = regressors.RFClassificationAlgorithm(feature_list, bin_num=20, min_radius=6.0, max_radius=28.0)
+#ispline_algo = reference_implementations.InterpolatingSplineAlgorithm()
+algos = [rf_algo, rfc_algo]
+samples = automatic_test.load_samples('data/training_data/samples.pickle')
+samples = automatic_test.normalize_features(samples)
+train_samples, test_samples = automatic_test.get_partitioned_samples(samples, 0.8)
+automatic_test.train(algos, train_samples)
+automatic_test.test(algos, test_samples)
+automatic_test.test_plot(algos, test_samples)
---
+++ tests/test3.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+#coding:utf-8
+# Systematic test of feature quality with RandomForestRegressor
+import automatic_test
+import regressors
+import reference_implementations
+
+feature_list = [
+    "intersection_angle",                       # Angle between entry and exit way
+    "maxspeed_entry",                           # Allowed maximum speed on entry way
+    "maxspeed_exit",                            # Allowed maximum speed on exit way
+    "lane_distance_entry_exact",                # Distance of track line to curve secant center point at 0 degree angle
+    "lane_distance_exit_exact",                 # Distance of track line to curve secant center point at 180 degree angle
+    "oneway_entry",                             # Is entry way a oneway street?
+    "oneway_exit",                              # Is exit way a oneway street?
+    "curvature_entry",                          # Curvature of entry way over INT_DIST
+    "curvature_exit",                           # Curvature of exit way over INT_DIST
+    "vehicle_speed_entry",                      # Measured vehicle speed on entry way at INT_DIST
+    "vehicle_speed_exit"                        # Measured vehicle speed on exit way at INT_DIST
+]
+samples = automatic_test.load_samples('data/training_data/samples.pickle')
+samples = automatic_test.normalize_features(samples)
+train_sample_sets, test_sample_sets = automatic_test.get_cross_validation_samples(samples, 0.7, 5)
+automatic_test.test_feature_permutations(
+                    regressors.RandomForestAlgorithm,
+                    train_sample_sets,
+                    test_sample_sets,
+                    feature_list,
+                    min_num_features=6,
+                    cross_validation=True)
