1372e63a5d8320e13d4f6d69c28576eead21f310
Christian Framing <cfshop@t-online.de>
2015-10-24 16:40:03 +0200

    Restructure rectification process and optionally allow for definition of entry and exit distance

--- rectify_prepared_data.py
+++ rectify_prepared_data.py
@@ -16,44 +16,49 @@ class RectificationError(Exception):
 def rectify_track(track, sample):
     track_line = LineString([(x,y) for x,y,time in track])
 
+    track_line = rectify_line(track_line, sample)
+    proj_coords = [transform_to_lon_lat(x, y) for (x, y) in track_line.coords[:]]
+    new_track = [(lon, lat, time) for ((lon,lat), (_,_,time)) in zip(proj_coords, track)]
+    return new_track
+
+def rectify_line(line, sample, des_entry_dist=None, des_exit_dist=None):
     entry_line = sample['geometry']['entry_line']
     exit_line = sample['geometry']['exit_line']
-    entry_normal = get_normal_to_line(entry_line, entry_line.length - INT_DIST)
-    exit_normal = get_normal_to_line(exit_line, INT_DIST)
-    entry_normal_vec = np.array(entry_normal.coords[1]) - np.array(entry_normal.coords[0])
-    exit_normal_vec = np.array(exit_normal.coords[1]) - np.array(exit_normal.coords[0])
+    entry_normal_vec = get_normal_vec_at(entry_line, entry_line.length - INT_DIST)
+    exit_normal_vec = get_normal_vec_at(exit_line, INT_DIST)
 
     oneway_entry = float_to_boolean(sample['X'][_feature_types.index('oneway_entry')])
     oneway_exit = float_to_boolean(sample['X'][_feature_types.index('oneway_exit')])
 
     # Calculate desired track distances
-    if oneway_entry:
-        desired_entry_distance = 0.
-    else:
-        desired_entry_distance = LANE_WIDTH/2
-    if oneway_exit:
-        desired_exit_distance = 0.
-    else:
-        desired_exit_distance = LANE_WIDTH/2
+    if des_entry_dist == None or des_exit_dist == None:
+        if oneway_entry:
+            des_entry_dist = 0.
+        else:
+            des_entry_dist = LANE_WIDTH/2
+        if oneway_exit:
+            des_exit_dist = 0.
+        else:
+            des_exit_dist = LANE_WIDTH/2
+
     max_position_error = 0.1    # Maximum deviation from desired value at entry and exit [m]
-    entry_error = lambda: get_lane_distance_projected_normal(entry_line, entry_line.length - INT_DIST, track_line) - desired_entry_distance
-    exit_error = lambda: get_lane_distance_projected_normal(exit_line, INT_DIST, track_line) - desired_exit_distance
+    entry_error = lambda: get_lane_distance_projected_normal(entry_line, entry_line.length - INT_DIST, line) - des_entry_dist
+    exit_error = lambda: get_lane_distance_projected_normal(exit_line, INT_DIST, line) - des_exit_dist
+
     max_iterations = 100    # Rectification failed if it is not reached after max_iterations
     current_iteration = 0
+
     while abs(entry_error()) > max_position_error or abs(exit_error()) > max_position_error:
         if current_iteration >= max_iterations:
-            raise RectificationError("Maximum iterations reached when optimizing track_line")
+            raise RectificationError("Maximum iterations reached when optimizing line")
         if abs(entry_error()) > max_position_error:
             translation_vec = - entry_normal_vec * entry_error()
-            track_line = shapely.affinity.translate(track_line, xoff=translation_vec[0], yoff=translation_vec[1])
+            line = shapely.affinity.translate(line, xoff=translation_vec[0], yoff=translation_vec[1])
         if abs(exit_error()) > max_position_error:
             translation_vec = - exit_normal_vec * exit_error()
-            track_line = shapely.affinity.translate(track_line, xoff=translation_vec[0], yoff=translation_vec[1])
+            line = shapely.affinity.translate(line, xoff=translation_vec[0], yoff=translation_vec[1])
         current_iteration += 1
-    print "Rectification successful."
-    proj_coords = [transform_to_lon_lat(x, y) for (x, y) in track_line.coords[:]]
-    new_track = [(lon, lat, time) for ((lon,lat), (_,_,time)) in zip(proj_coords, track)]
-    return new_track
+    return line
 
 if __name__ == "__main__":
     for fn in sys.argv[1:]:
